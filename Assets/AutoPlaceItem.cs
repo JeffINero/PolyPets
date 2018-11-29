2f9af13225e3526cbe9f4081b9fd599235a37963
Peter Nauroth <petnaur@web.de>
2018-11-29 19:05:15 +0100

    second commit

---
+++ Assets/AutoPlaceItem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARSessionOrigin))]
+public class AutoPlaceItem : MonoBehaviour
+{
+
+    [SerializeField]
+    //[Tooltip("Instantiates this prefab on a plane at the touch Location.")]
+    GameObject GameObjectToPlace;
+
+
+    ARSessionOrigin m_SessionOrigin;
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    void Awake()
+    {
+        m_SessionOrigin = GetComponent<ARSessionOrigin>();
+    }
+
+
+    void Update()
+    {
+
+        if (!GameObjectToPlace.active&&m_SessionOrigin.Raycast(Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f)), s_Hits, TrackableType.PlaneWithinPolygon))
+        {
+            Pose hitPose = s_Hits[0].pose;
+            GameObjectToPlace.transform.position = hitPose.position;
+            GameObjectToPlace.SetActive(true);
+        }
+       
+
+    }
+}
---
+++ Assets/PlaceOnPlan.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARSessionOrigin))]
+public class PlaceOnPlan : MonoBehaviour {
+
+    [SerializeField]
+    //[Tooltip("Instantiates this prefab on a plane at the touch Location.")]
+    GameObject m_PlacedPrefab;
+
+    public GameObject placedPrefab
+    {
+        get { return m_PlacedPrefab; }
+        set { m_PlacedPrefab = value; }
+    }
+
+
+    public GameObject spawnedObject { get; private set;}
+
+    ARSessionOrigin m_SessionOrigin;
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+	void Awake () {
+        m_SessionOrigin = GetComponent<ARSessionOrigin>();  
+    }
+	
+
+	void Update () {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (m_SessionOrigin.Raycast(Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0)), s_Hits, TrackableType.PlaneWithinPolygon))
+            {
+                
+                Pose hitPose = s_Hits[0].pose;
+                if(spawnedObject == null)
+                {
+                    spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
+                }
+                else
+                {
+                    spawnedObject.transform.position = hitPose.position;
+                }
+                if (!spawnedObject.active)
+                {
+                    spawnedObject.SetActive(true);
+                }
+            }
+        }
+		
+	}
+}
